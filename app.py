6549ae9d6b23ad70969ba0be4dd9350bd881b0a6
Daniel Gilbert <loglow@gmail.com>
2018-02-28 16:32:47 -0500

    testing docker autobuild on push

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = "<h3>This is where the hotline will be!</h3>" \
+    html = "<h3 style='color:red;'>This is where the hotline will be!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>"
     return html.format(hostname=socket.gethostname())
 
